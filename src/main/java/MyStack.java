9b71b2207671d274fe2e54088708e7a9b52157d8
Mauricio MdS <45921381+Mauricio-MdS@users.noreply.github.com>
2024-10-08 11:35:57 -0300

    Implements stack with queue

---
+++ src/main/java/MyStack.java
@@ -0,0 +1,30 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+class MyStack {
+
+    Queue<Integer> queue;
+
+    public MyStack() {
+        queue = new LinkedList<>();
+    }
+
+    public void push(int x) {
+        queue.offer(x);
+        int size = queue.size();
+
+        while (size-- > 1) queue.offer(queue.poll());
+    }
+
+    public int pop() {
+        return queue.poll();
+    }
+
+    public int top() {
+        return queue.peek();
+    }
+
+    public boolean empty() {
+        return queue.isEmpty();
+    }
+}
